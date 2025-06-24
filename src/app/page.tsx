b541b712497146bdaa2405ee073174d969204340
TechieTeee <techieteee@protonmail.com>
2025-06-24 03:23:42 +0000

    Updated starter questions for survey with labels and new type inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
-
-import { useForm, SubmitHandler } from "react-hook-form"
-
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
-  example: string
-  exampleRequired: string
-}
-
+  firstName: string;
+  lastName: string;
+};
 
 export default function App() {
   const {
@@ -15,27 +12,26 @@ export default function App() {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-
-  console.log(watch("example")) // watch input value by passing the name of it
-
+  console.log(watch("firstName")); // watch input value by passing the name of it
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-
+      {/* Label for First Name */}
+      <label>First Name</label>
+      {/* Registers Value for First Name*/}
+      <input defaultValue="John" {...register("firstName")} />
 
       {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
+         {/* Label for Last Name */}
+      <label>Last Name</label>
+      <input {...register("lastName", { required: true })} />
       {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-
-
+      {errors.lastName && <span>Last Name Required</span>}
       <input type="submit" />
     </form>
-  )
+  );
 }
